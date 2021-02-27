f768a304a5ba35cff3544543cd0062b4d596c0f2
Lili Huynh <lillianhuynh312@gmail.com>
2021-02-26 17:36:11 -0800

    access pending object store,add record to your store with add method

--- public/db.js
+++ public/db.js
@@ -3,16 +3,31 @@ let db;
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
-  // create object store called "pending" and set autoIncrement to true
-  const db = event.target.result;
-  db.createObjectStore("pending", { autoIncrement: true });
+    // create object store called "pending" and set autoIncrement to true
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
 };
 
 request.onsuccess = function (event) {
     db = event.target.result;
-  
+
     // check if app is online before reading from db
     if (navigator.onLine) {
-      checkDatabase();
+        checkDatabase();
     }
-  };
+};
+
+request.onerror = function (event) {
+    console.log("Woops! " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+    // open a transaction on the pending object store in the budget db with readwrite access
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    // access your pending object store
+    const store = transaction.objectStore("pending");
+
+    // add record to your store with add method.
+    store.add(record);
+}
